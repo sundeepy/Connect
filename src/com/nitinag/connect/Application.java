1f9143ebe9f9871227b36f483a7e00dd350cb0a3
Nitin Agarwal <nitinag@yahoo-inc.com>
2014-09-29 23:14:49 -0700

    Login/SignUP using parse

---
+++ src/com/nitinag/connect/Application.java
@@ -0,0 +1,62 @@
+package com.nitinag.connect;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.nitinag.connect.model.AnywallPost;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class Application extends android.app.Application {
+  // Debugging switch
+  public static final boolean APPDEBUG = false;
+
+  // Debugging tag for the application
+  public static final String APPTAG = "Connect";
+
+  // Used to pass location from MainActivity to PostActivity
+  public static final String INTENT_EXTRA_LOCATION = "location";
+
+  // Key for saving the search distance preference
+  private static final String KEY_SEARCH_DISTANCE = "searchDistance";
+
+  private static final float DEFAULT_SEARCH_DISTANCE = 250.0f;
+
+private static final String PARSE_APPLICATION_ID = "nh7qch34SMCs00ySmhBEuZWczFnpfbDp0WYomSY6";
+
+private static final String PARSE_CLIENT_KEY = "ED4eq5TDpyOVQjBWJQ6SX2t6pxA2lH4W9SSptOMD";
+
+  private static SharedPreferences preferences;
+
+  private static ConfigHelper configHelper;
+
+  public Application() {
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    ParseObject.registerSubclass(AnywallPost.class);
+    Parse.initialize(this, PARSE_APPLICATION_ID,
+        PARSE_CLIENT_KEY);
+
+    preferences = getSharedPreferences("com.nitinag.connect", Context.MODE_PRIVATE);
+
+    configHelper = new ConfigHelper();
+    configHelper.fetchConfigIfNeeded();
+  }
+
+  public static float getSearchDistance() {
+    return preferences.getFloat(KEY_SEARCH_DISTANCE, DEFAULT_SEARCH_DISTANCE);
+  }
+
+  public static ConfigHelper getConfigHelper() {
+    return configHelper;
+  }
+
+  public static void setSearchDistance(float value) {
+    preferences.edit().putFloat(KEY_SEARCH_DISTANCE, value).commit();
+  }
+
+}
